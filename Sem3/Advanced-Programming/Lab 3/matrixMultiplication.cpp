36e4cf4ef534acc0e821ee4fe4471622f073846d
Praneesh R V <praneeshrv404@gmail.com>
2024-08-05 16:31:08 +0530

    matrixmultopoverload

--- Sem3/Advanced-Programming/Lab 3/matrixMultiplication.cpp
+++ Sem3/Advanced-Programming/Lab 3/matrixMultiplication.cpp
@@ -1,72 +1,95 @@
 #include <iostream>
 using namespace std;
 
-class Matrix{
+class Matrix {
     private:
     int row;
     int col;
-    int a,b;
     int **data;
 
     public:
-    Matrix(int r,int c){
-        row=r;
-        col=c;
+    Matrix(int r, int c) {
+        row = r;
+        col = c;
         data = new int*[r];
-        for(int i =0;i< r ; ++i){
+        for (int i = 0; i < r; ++i) {
             data[i] = new int[c];
-            for(int j = 0;j<c;++j){
+            for (int j = 0; j < c; ++j) {
                 data[i][j] = 0;
             }
         }
     }
 
-    void setvalue(int r,int c, int value){
-        data[r][c]=value;
+    void setvalue(int r, int c, int value) {
+        data[r][c] = value;
     }
 
-    Matrix operator*(Matrix const &other){
-        Matrix result(row,col);
-        for(int i = 0; i < row; i++)
-        {
-            for(int j = 0; j < col; j++)
-            {
-                for(int k = 0; k < row; k++)
-                {
+    Matrix operator*(const Matrix& other) const {
+        if (col != other.row) {
+            cout<<"Matrix dimensions are not compatible for multiplication."<<endl;
+        }
+        
+        Matrix result(row, other.col);
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < other.col; j++) {
+                for (int k = 0; k < col; k++) {
                     result.data[i][j] += data[i][k] * other.data[k][j];
                 }
             }
         }
         return result;
-
     }
-    void print(){
-    for(int k = 0; k < row; k++ )
-    {
-        for(int l = 0; l < col; l++)
-        {
-            cout<<data[k][l]<<" ";
+
+    void print() {
+        for (int k = 0; k < row; k++) {
+            for (int l = 0; l < col; l++) {
+                cout << data[k][l] << " ";
+            }
+            cout << endl;
         }
-        cout<<endl;
     }
-    }
-
 };
 
-int main(){
-    Matrix m1(2,2);
-    Matrix m2(2,2);
-    m1.setvalue(0,0,2);
-    m1.setvalue(0,1,3);
-    m1.setvalue(1,0,5);
-    m1.setvalue(1,1,2);
+int main() {
+    int rows1, cols1, rows2, cols2;
 
-    m2.setvalue(0,0,5);
-    m2.setvalue(0,1,3);
-    m2.setvalue(1,0,2);
-    m2.setvalue(1,1,1);
+    cout << "Enter dimensions for matrix 1 (rows columns): ";
+    cin >> rows1 >> cols1;
+    cout << "Enter dimensions for matrix 2 (rows columns): ";
+    cin >> rows2 >> cols2;
 
-    Matrix m3 = m1*m2;
-    m3.print();
+    if (cols1 != rows2) {
+        cout << "Error: The number of columns in the first matrix must equal the number of rows in the second matrix." << endl;
+        return 1;
+    }
+
+    Matrix m1(rows1, cols1);
+    Matrix m2(rows2, cols2);
+
+    cout << "Enter values for matrix 1:" << endl;
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols1; j++) {
+            int value;
+            cout << "Enter value for m1[" << i << "][" << j << "]: ";
+            cin >> value;
+            m1.setvalue(i, j, value);
+        }
+    }
+
+    cout << "Enter values for matrix 2:" << endl;
+    for (int i = 0; i < rows2; i++) {
+        for (int j = 0; j < cols2; j++) {
+            int value;
+            cout << "Enter value for m2[" << i << "][" << j << "]: ";
+            cin >> value;
+            m2.setvalue(i, j, value);
+        }
+    }
+
+
+        Matrix m3 = m1 * m2;
+        cout << "Result of matrix multiplication:" << endl;
+        m3.print();
+
     return 0;
 }
