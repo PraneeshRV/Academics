cf6cbc010c27c0b947ec1bc109f762023e1fdde0
Praneesh R V <praneeshrv404@gmail.com>
2024-08-19 09:38:16 +0530

    fcfs

--- Sem3/OS Lab/Lab 4/fcfs.c
+++ Sem3/OS Lab/Lab 4/fcfs.c
@@ -2,7 +2,6 @@
 
 #define MAX_PROCESSES 4
 
-// Structure to represent a process
 struct Process {
     int id;
     int arrival_time;
@@ -12,31 +11,24 @@ struct Process {
     int waiting_time;
 };
 
-// Function to calculate times for FCFS scheduling
 void calculateTimes(struct Process processes[], int n) {
     int current_time = 0;
 
     for (int i = 0; i < n; i++) {
-        // If current time is less than arrival time, update current time
         if (current_time < processes[i].arrival_time) {
             current_time = processes[i].arrival_time;
         }
 
-        // Calculate completion time
         processes[i].completion_time = current_time + processes[i].burst_time;
 
-        // Calculate turnaround time
         processes[i].turnaround_time = processes[i].completion_time - processes[i].arrival_time;
 
-        // Calculate waiting time
         processes[i].waiting_time = processes[i].turnaround_time - processes[i].burst_time;
 
-        // Update current time
         current_time = processes[i].completion_time;
     }
 }
 
-// Function to display the process details
 void displayProcesses(struct Process processes[], int n) {
     printf("\nProcess\tArrival Time\tBurst Time\tCompletion Time\tTurnaround Time\tWaiting Time\n");
     for (int i = 0; i < n; i++) {
@@ -58,10 +50,9 @@ int main() {
         {4, 4, 5}
     };
 
-    // Calculate times
     calculateTimes(processes, MAX_PROCESSES);
 
-    // Display results
+
     displayProcesses(processes, MAX_PROCESSES);
     
     // Calculate and display average waiting time and turnaround time
