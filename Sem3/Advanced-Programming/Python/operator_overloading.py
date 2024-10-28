767f0696ebe26114b6c34d83cfd9c92024a8e1cc
Praneesh R V <praneeshrv404@gmail.com>
2024-10-28 10:02:16 +0530

    operator overload

--- Sem3/Advanced-Programming/Python/operator_overloading.py
+++ Sem3/Advanced-Programming/Python/operator_overloading.py
@@ -6,7 +6,12 @@ class Point:
     def __add__(self,other):
         return Point(self.x+other.x,self.y+other.y)
 
+    def __mul__(self,other):
+        return Point(self.x*other.x,self.y*other.y)
+    
 P1=Point(5,8)
 P2=Point(2,5)
 P3=P1+P2
 print(f"({P3.x},{P3.y})")
+P4=P1*P2
+print(f"({P4.x},{P4.y})")
