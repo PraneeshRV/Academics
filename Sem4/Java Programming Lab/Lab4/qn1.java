efcf0dc9db950449f272e5d1a9fc4243c95887da
Praneesh R V <praneeshrv404@gmail.com>
2025-01-06 15:30:53 +0530

    java

---
+++ Sem4/Java Programming Lab/Lab4/qn1.java
@@ -0,0 +1,42 @@
+
+import java.util.Scanner;
+
+class Vehicle {
+    protected String type;
+    
+    public Vehicle(String type) {
+        this.type = type;
+    }
+    
+    public void displayType() {
+        System.out.println("Type: " + type);
+    }
+}
+
+class Car extends Vehicle {
+    private String brand;
+    
+    public Car(String type, String brand) {
+        super(type);
+        this.brand = brand;
+    }
+    
+    public void displayBrand() {
+        System.out.println("Brand: " + brand);
+    }
+}
+
+public class qn1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        String type = scanner.nextLine();
+        String brand = scanner.nextLine();
+        
+        Car car = new Car(type, brand);
+        car.displayType();
+        car.displayBrand();
+        
+        scanner.close();
+    }
+}
