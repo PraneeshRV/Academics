fe459b930711d68bfe9ad8ceb4cb755c72426f1c
Praneesh R V <praneeshrv404@gmail.com>
2025-01-07 16:28:29 +0530

    technical cir

---
+++ Sem4/Technical Session CIR/Session 4/Qn3.cpp
@@ -0,0 +1,17 @@
+// C++ program to find the word in a given string that has the highest number of repeated letters
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+    string word = "";
+    string highest_word = "";
+    int max_repeated_letters = 0;
+    int repeated_letters =0;
+
+    return 0;
+}
