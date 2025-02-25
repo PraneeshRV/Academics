1e4b04f088f60395e038376c30142d433c0c54f2
PraneeshRV <praneeshrv404@gmail.com>
2025-02-25 11:18:27 +0530

    quicksort

---
+++ Sem4/DSA/Programs/quicksort.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[100], n;
+    
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    
+    printf("Enter %d elements: ", n);
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    
+    printf("Original array: ");
+    printArray(arr, n);
+    
+    quickSort(arr, 0, n - 1);
+    
+    printf("Sorted array: ");
+    printArray(arr, n);
+    
+    return 0;
+}
